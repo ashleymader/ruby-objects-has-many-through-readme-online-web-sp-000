4415aa1a4a120672a8547e1ed7d61f1ae9b7335b
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-18 09:45:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -30,6 +30,13 @@ class Waiter
   best_tipped_meal.customer
   end
   
+  def worst
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
+  best_tipped_meal.customer
+  end
+  
   
     
 end
